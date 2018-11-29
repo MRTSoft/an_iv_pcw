b1e141a1ca8138be70b73877c8198ad7a83830e3
Tudor Marcu <contact.tudor.marcu@gmail.com>
2018-11-29 15:01:45 +0200

    Retinem raspunsurile

--- pcw_lab08/chestionar/app.js
+++ pcw_lab08/chestionar/app.js
@@ -1,16 +1,26 @@
 var app = angular.module('main', []);
 
 app.controller('ctrl', function($scope, $http) {
-    $scope.db = [1, 2, 3];
+    $scope.uAns = {};
     $http.get("intrebari.json", {
         responseType : "json"
     })
     .then(function (response) {
     	$scope.db = response.data.intrebari;
     	console.debug($scope.db);
-    	for(i = 1; i<= $scope.db.length; ++i) {
-			$scope.db[i-1].id = i;
+    	for(i = 0; i< $scope.db.length; ++i) {
+			$scope.db[i].id = i;
+			if ($scope.db[i].tip == 'multiplu'){
+				$scope.db[i].uAns = [];
+			}
+			else {
+				$scope.db[i].uAns = undefined;
+			}
 		}
     	
     })
+    
+    $scope.calculateScore = function () {
+		console.log($scope.db);
+	}
 });
