2ab61258365cb719cd88fa10534288e5f2e0410e
Tudor Marcu <contact.tudor.marcu@gmail.com>
2018-11-29 11:39:29 +0200

    Update

--- pcw_lab08/chestionar/app.js
+++ pcw_lab08/chestionar/app.js
@@ -1,16 +1,16 @@
 var app = angular.module('main', []);
 
 app.controller('ctrl', function($scope, $http) {
-    $scope.qTitle= "Ce se intampla doctore?";
-    $scope.qCurrentId= "2";
-    $scope.qTotal= "7";
-    $scope.cTextVisible= "visible";
     $scope.db = [1, 2, 3];
     $http.get("intrebari.json", {
         responseType : "json"
     })
     .then(function (response) {
     	$scope.db = response.data.intrebari;
-    	console.debug($scope.db);    	
+    	console.debug($scope.db);
+    	for(i = 1; i<= $scope.db.length; ++i) {
+			$scope.db[i-1].id = i;
+		}
+    	
     })
 });
