6947c6ae790d2635b886ac90b4e2dec685ab1f3a
TudorMarcu <contact.tudor.marcu@gmail.com>
2018-12-06 13:37:35 +0200

    Terminat chestionar

--- pcw_lab08/chestionar/app.js
+++ pcw_lab08/chestionar/app.js
@@ -2,6 +2,7 @@ var app = angular.module('main', []);
 
 app.controller('ctrl', function($scope, $http) {
     $scope.uAns = {};
+    $scope.showScore = 'none';
     $http.get("intrebari.json", {
         responseType : "json"
     })
@@ -21,6 +22,56 @@ app.controller('ctrl', function($scope, $http) {
     })
     
     $scope.calculateScore = function () {
-		console.log($scope.db);
+        $scope.score = 0.0;
+        console.log($scope.db);
+		for(qi in $scope.db){
+            q = $scope.db[qi];
+            console.log(q);
+            if (q.uAns != undefined){
+                if (q.tip == "numeric"){
+                    if (q.uAns == q.corecte[0]){
+                        $scope.score += 1.0;
+                    }
+                }
+                if (q.tip == "text"){
+                    a = q.uAns.replace(/\s/g, '').toLowerCase();
+                    b = q.corecte[0].replace(/\s/g, '').toLowerCase();
+                    if (a == b){
+                        $scope.score += 1.0;
+                    }
+                }
+                if (q.tip == "simplu"){
+                    if (q.uAns == q.corecte[0])
+                        $scope.score += 1.0;
+                }
+                if (q.tip == "multiplu"){
+                    for (i = 0; i<q.raspunsuri.length; ++i){
+                        if (q.uAns[i] == undefined){
+                            q.uAns[i] = false;
+                        }
+                    }
+                    correct = true;
+                    for (a = 0; a < q.uAns.length && correct == true; a = a+1){
+                        if (q.uAns[a]){
+                            if (q.corecte.indexOf(q.raspunsuri[a]) == -1){
+                                correct = false;
+                            }
+                        } else {
+                            if (q.corecte.indexOf(q.raspunsuri[a]) != -1){
+                                correct = false;
+                            }
+                        }
+                    }
+                    if (correct){
+                        $scope.score += 1.0;
+                    }
+                }//if multiplu
+            }//if uAns != undefined
+            console.log(qi);
+            console.log($scope.score);
+        }//for each question
+
+        $scope.score = $scope.score / $scope.db.length * 100.0;
+        $scope.showScore = 'inherit';
 	}
 });
