7a33601d45836263cb72ccbe48086a1cf5c287a1
Tudor Marcu <tudorhome2@gmail.com>
2018-12-11 02:17:50 +0200

    Server now uses sockets

--- pcw_lab09/Tanks/server.js
+++ pcw_lab09/Tanks/server.js
@@ -1,18 +1,22 @@
-var http = require("http");
+var http = require("http").createServer(handler);
 var fs = require('fs');
+var io = require('socket.io')(http);
 var clientQueue = [];
 
-http.createServer(function (request, response) {
+
+function handler(request, response) {
    // Send the HTTP header 
    // HTTP Status: 200 : OK
    // Content Type: text/plain
-	console.log(request.url);
+	//console.log(request.url);
 	if (request.url.startsWith('/api')){
 		handleAPICall(request, response);
 	} else {
 		handleFileRequest(request, response);
 	}
-}).listen(8081);
+}
+
+http.listen(8081);
 
 
 function handleFileRequest(request, response){
@@ -50,3 +54,26 @@ function handleAPICall(request, response) {
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
+
+// Socket code
+
+io.on('connection', function(socket){
+  console.log('A user connected: ' + socket.id);
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('join', function(data){ handleJoin(socket, data); });
+});
+
+function handleJoin(socket, data){
+	console.log('A new client is joining. ' + socket.id);
+	if (!data){
+		console.log(data.toString());
+	}
+}
+
+var serverData = {
+	userQueue : {},
+	ongoingGames : {},
+}
